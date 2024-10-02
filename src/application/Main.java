6fbf6e3871615ffa2f19d79d8e615cfb40a6b1d9
Lucas Bandeira <lucasbandeirai40@gmail.com>
2024-10-02 15:41:02 -0300

    Feat: Add Seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,12 +2,24 @@ package application;
 
 import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
 
-import java.sql.Connection;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 
 public class Main {
     public static void main( String[] args ) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         Department department = new Department(1,"Books");
+        Seller seller = new Seller(34,
+                "John Marston",
+                "John@gmail.com",
+                LocalDate.parse("19/02/1873",dateTimeFormatter),
+                3000.0,
+                department);
+
         System.out.println(department);
+        System.out.println(seller);
     }
 }
